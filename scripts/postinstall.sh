0b838cf6909b429c6840faa594daf6d5f0ff5a50
Karim Radhouani <medkarimrdi@gmail.com>
2023-11-11 14:16:46 -0800

    update install scripts

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sr_cli tools system app-management application app_mgr reload
+appmgr=$(sr_linux --status | grep app_mgr | cut -d: -f 2 | tr -s ' ')
+
+if [[ $appmgr != "not running" ]]
+then
+    sr_cli tools system app-management application app_mgr reload
+fi
